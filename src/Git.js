3afed4738fba2d84d6596f68c171612e37c49d98
veramsteixeira <129678252+veramsteixeira@users.noreply.github.com>
2023-06-22 17:33:48 +0100

    git link

--- src/Git.js
+++ src/Git.js
@@ -4,10 +4,8 @@ import React from "react";
 export default function Git() {
     return (
       <div className="Git">
-       
+       <a href="https://github.com/panizAR/weather">GitHub repository</a>
       </div>
-
-      <a ref={}
     );
   }
   
